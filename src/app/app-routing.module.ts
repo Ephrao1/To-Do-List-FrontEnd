2c9fd2355db4ed0e3958058fa30239906e972a63
Ephraim Bundi <ebundi04@gmail.com>
2021-09-30 18:28:06 +0300

    Modify moduke.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component'
+import { TodolistComponent } from './todolist/todolist.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app-register', component: RegisterComponent},
+  {path: 'app-login', component: LoginComponent},
+  {path: 'app-todolist', component: TodolistComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
